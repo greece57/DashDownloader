542c113d4dc2913e12bac2af24e435bfde973b99
Niko <greece93@gmail.com>
2016-02-06 14:05:28 +0200

    init commit

---
+++ src/DashDownloader.java
@@ -0,0 +1,9 @@
+
+public class DashDownloader {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ src/MPGParser.java
@@ -0,0 +1,11 @@
+
+public class MPGParser {
+  public MPGInfos parseFile(String mpgFile)
+  {
+    MPGInfos info = new MPGInfos();
+    
+    // parse mpgFile and return info
+    
+    return info;
+  }
+}
